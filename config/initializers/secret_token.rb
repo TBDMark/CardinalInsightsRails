3b59e3628d6ef95e6ef0cc6a62b8fccc9e4e0add
TBDMark <mark@twobytesdata.com>
2014-01-15 13:13:54 -0500

    Initial Setup Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CardinalInsights::Application.config.secret_key_base = '61b26f071231f903463ece20f9e9fbcecdb4af90b994e8e5627cfc22d98a7a37f120585a95e4def434fa50eca8456df38af30498e34ed94a908ad9a5abc1dea4'
